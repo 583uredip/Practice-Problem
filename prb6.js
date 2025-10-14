c46c92532cce26295a98c921dfbc9bf0761306b1
Redip Biswas Partho <redipbiswas8@gmail.com>
2025-10-15 02:14:10 +0600

    Add files via upload

---
+++ prb6.js
@@ -0,0 +1,10 @@
+let x=parseInt(prompt("Enter the coordinate of your friend's house:"));
+if(isNaN(x) || x<1)
+{
+alert("Please enter a valid positive number!");
+}
+else
+{
+let steps = Math.ceil(x/5);
+alert("Minimum number of steps:" + steps);
+}
