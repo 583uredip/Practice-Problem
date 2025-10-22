f00fc475d81d221f781deb76090f305fe8c75635
Redip Biswas Partho <redipbiswas8@gmail.com>
2025-10-22 10:11:15 +0600

    Add files via upload

---
+++ prb9.js
@@ -0,0 +1,22 @@
+let s=prompt("Enter Your String");
+let lower=0;
+let uppar=0;
+for(let i=0;i<s.length;i++)
+{
+if(s[i]>='A' && s[i]<='B')
+{
+uppar++;
+}
+else
+{
+lower++;
+}
+}
+if(uppar>lower)
+{
+console.log(s.toUpperCase());
+}
+else
+{
+console.log(s.toLowerCase());
+}
