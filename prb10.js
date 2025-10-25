de8a65cee406e11cfc807e858eb92a6cb702b525
Redip Biswas Partho <redipbiswas8@gmail.com>
2025-10-26 00:32:06 +0600

    Add files via upload

---
+++ prb10.js
@@ -0,0 +1,14 @@
+let n=parseInt(prompt("Enter Your Number:"))
+let k=parseInt(prompt("Enter The Number Of Subtraction"));
+for(let i=0;i<k;i++ )
+{
+if(n%10===0)
+{
+n=n/10;
+}
+else
+{
+n=n-1;
+}
+}
+alert("The Answer Is:"+n);
